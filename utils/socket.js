5882461df207d1241b89c4f7917a52343b779e32
Garima <yadavgarima451@gmail.com>
2026-04-11 20:02:55 +0530

    fix: changes in socket.js

--- utils/socket.js
+++ utils/socket.js
@@ -1,72 +1,25 @@
-// const { Server } = require("socket.io");
-// const Message = require("../src/models/message");
 
-// const initializeSocket = (server) => {
-//   const io = new Server(server, {
-//     cors: {
-//       origin: "http://localhost:5173",
-//       methods: ["GET", "POST"],
-//       credentials: true,
-//     },
-//   });
-
-//   io.on("connection", (socket) => {
-//     console.log("⚡ User connected: " + socket.id);
-
-//     // Join chat room based on user IDs to ensure both users are in the same room
-//     socket.on("joinChat", ({ senderId, receiverId }) => {
-//       const room = [senderId, receiverId].sort().join('-');
-//       socket.join(room);
-//       console.log(`👥 User joined room: ${room}`);
-//     });
-
-//     // Handle sending and saving messages
-//     socket.on("sendMessage", async ({ connectionId, senderId, receiverId, text }) => {
-//       console.log("📩 Received sendMessage event...");
-//       console.log("Data:", { connectionId, senderId, receiverId, text });
-
-//       try {
-//         // Save to MongoDB
-//         const message = new Message({
-//           senderId,
-//           receiverId,
-//           connectionId,
-//           text,
-//         });
-
-//         const savedMessage = await message.save();
-//         console.log("✅ Message saved to MongoDB:", savedMessage._id);
-
-//         // Broadcast to the room
-//         const room = [senderId, receiverId].sort().join('-');
-//         io.to(room).emit("messageReceived", savedMessage);
-//         console.log("📣 Message broadcasted to room:", room);
-
-//       } catch (err) {
-//         console.error("❌ Error saving message:", err.message);
-//       }
-//     });
-
-//     socket.on("disconnect", () => {
-//       console.log("❌ User disconnected");
-//     });
-//   });
-// };
-
-// module.exports = initializeSocket;
 
 
 const { Server } = require("socket.io");
 const Message = require("../src/models/message");
 
 const initializeSocket = (server) => {
+  // const io = new Server(server, {
+  //   cors: {
+  //     origin: "http://localhost:5173",
+  //     methods: ["GET", "POST"],
+  //     credentials: true,
+  //   },
+  // });
   const io = new Server(server, {
-    cors: {
-      origin: "http://localhost:5173",
-      methods: ["GET", "POST"],
-      credentials: true,
-    },
-  });
+  cors: {
+    // Add your AWS IP here
+    origin: ["http://localhost:5173", "http://13.60.253.32"],
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
 
   io.on("connection", (socket) => {
     console.log("⚡ User connected: " + socket.id);
